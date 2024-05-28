b83dda45189014774302ac0dae1ac18b24b68600
Michael Hudson <michaelhudson2205@gmail.com>
2024-05-28 20:00:46 +1000

    List imports. Prepare empty psvm.

--- src/degreePlanningGraphApp/DegreeApp.java
+++ src/degreePlanningGraphApp/DegreeApp.java
@@ -1,6 +1,23 @@
 package degreePlanningGraphApp;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Scanner;
+import.util.stream.Collectors;
+
 public class DegreeApp
 {
-
-}
+	public static void main(String[] args)
+	{
+		
+	} // >>>>>>>>>>end of psvm<<<<<<<<<<
+} // >>>>>>>>>>end of class DegreeApp<<<<<<<<<<
