bb13fc1d068b350c23077ad86fc14e668c3f5bc2
Michael Hudson <michaelhudson2205@gmail.com>
2024-05-29 20:32:58 +1000

    Create scanner and ask for input.

--- src/degreePlanningGraphApp/DegreeApp.java
+++ src/degreePlanningGraphApp/DegreeApp.java
@@ -12,12 +12,33 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Scanner;
-import.util.stream.Collectors;
+import java.util.stream.Collectors;
 
 public class DegreeApp
 {
 	public static void main(String[] args)
 	{
+		// Set up for getting user input
+		String path = "C:\\Users\\micha\\Dropbox\\Uni_SA\\COMP2033\\Assessments\\A_2_2\\";
+		Scanner scanner = new Scanner(System.in);
+		
+		// Ask the student for the name of the input file
+		System.out.print("Enter the name of the input file: ");
+		String fileName = scanner.nextLine();
+		path += fileName;
+		
+		// Ask the student for the maximum number of units they can study concurrently
+		int maxUnits = 0;
+		while (maxUnits < 1 || maxUnits > 4)
+		{
+			System.out.print("Enter the maximum number of units you can study concurrently (1-4): ");
+			maxUnits = scanner.nextInt();
+			if (maxUnits < 1 || maxUnits > 4)
+			{
+				System.out.println("Invalid input. Please enter a value between 1 and 4.");
+			}
+		}
+		scanner.close();
 		
 	} // >>>>>>>>>>end of psvm<<<<<<<<<<
 } // >>>>>>>>>>end of class DegreeApp<<<<<<<<<<
